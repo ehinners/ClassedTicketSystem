4fb7cfa5747fb3654d4fea6225d30629d2810742
ehinners <ehinners@my.wctc.edu>
2021-10-03 07:45:10 -0500

    Init Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using NLog.Web;
+using System.Collections.Generic;
+
+namespace ClassedTicketSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("");
+
+            //////////////////////////////
+            //      NLOG Instantiation  //
+            //////////////////////////////
+
+            string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+
+            // create instance of Logger
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog(path).GetCurrentClassLogger();
+
+            logger.Info("NLOG Loaded");
+        }
+    }
+}
